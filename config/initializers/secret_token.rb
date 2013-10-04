2cf840e15b2802511d763cdbf1b8b92d2e91df5d
KazukiYunoue <you.know.ue1480@gmail.com>
2013-10-04 11:17:11 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DynamicGridTest::Application.config.secret_token = 'd931f4b1e60b0dd7f844b2828dec96f764f147cc1ca96b518854f4b0f257ec816ebf668646651ac714e963639ea4faadcd8f36cde6d44b8fb59c102a14a076ee'
